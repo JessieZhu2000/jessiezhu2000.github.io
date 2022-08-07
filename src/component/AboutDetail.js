8084def59cbcad76a55bae9471664fcd677c3f0f
Jessie Zhu <jzhuuk@gmail.com>
2022-08-07 19:28:39 +0100

    update location wording

--- src/component/AboutDetail.js
+++ src/component/AboutDetail.js
@@ -12,7 +12,7 @@ export default () => {
                 We are available seven days a week, including bank holidays at short notice! We offer competitive prices yet we never compromise on our standard!
             </h2>
             <h2>
-                We offer professional dog walking services in-and-around Cambridgeshire. Please check out location page for more details about which areas we operation in.
+                We offer professional dog walking services in-and-around Cambridgeshire. Please check out location page for more details about which areas we operate in.
             </h2>
             <h2>
                 While we are walking, we will provide you with instant WhatsApp updates and we will provide plenty of exercises, games and socialising time for your dog.
